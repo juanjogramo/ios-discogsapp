824dcbb8d8c306b6d0790fa59ebf5409423df33f
Juan Jose Granados <juanjogramo@gmail.com>
2024-12-02 21:37:04 -0500

    Initial Commit

---
+++ DiscogsApp/DiscogsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscogsAppApp.swift
+//  DiscogsApp
+//
+//  Created by Juan José Granados Moreno on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DiscogsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
